eb62c15ba881b614ff33cb5881382f9fe6d05bd5
Benjamin Mosher <cyborg5989@hotmail.com>
2018-04-30 23:52:06 -0700

    Add new AI spawn structure

---
+++ TheRoost/Assets/Scripts/MonoBehaviors/AiShipObjectSpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiShipObjectSpawn : MonoBehaviour 
+{
+	public enum AiObjects
+	{
+		Gunship
+	}
+
+	[SerializeField]
+	public AiObjects AiObject;
+
+	public ShipEntry Entry
+	{
+		get
+		{
+			ShipEntry entry = null;
+			switch(AiObject)
+			{
+				case AiObjects.Gunship:
+					entry = ShipEntry.Gunship;
+					break;
+			}
+			return entry;
+		}
+	}
+}
